b12220433719a182fb814e450cea9569f8adb47b
TheGreatFruit <pierre.28.gauthier@gmail.com>
2026-04-16 17:16:23 +0200

    ITER 1

---
+++ src/tdd/Game.java
@@ -0,0 +1,13 @@
+package tdd;
+
+public class Game {
+
+	public void roll(int nbQuilles) {
+		// Bonjour...
+	}
+
+	public int score() {
+		return 0;
+	}
+
+}
---
+++ test/test/GameTest.java
@@ -0,0 +1,33 @@
+package test;
+import tdd.Game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class GameTest {
+	
+	Game game;
+	
+	
+	@BeforeEach
+	void init() {
+		game = new Game();
+	}
+	
+	@Test
+	void test_vide() {
+		assertEquals(0, game.score());
+	}
+	
+	@Test
+	void test_aucune_quille() {
+		for(int i=0; i<20; i++) {
+			game.roll(0);
+		}
+			assertEquals(0, game.score());
+	}
+	
+}
