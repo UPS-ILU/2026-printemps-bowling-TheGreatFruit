13c50ecae7386220b7bdee2c02e326b473fa56f6
TheGreatFruit <pierre.28.gauthier@gmail.com>
2026-04-16 17:46:02 +0200

    test_spare

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -2,10 +2,29 @@ package tdd;
 
 public class Game {
 	
+	private int turn = 1;
 	private int score = 0;
+	private int last_throw = 0;
+	private int multiplier = 1;
+	private int multiplier_turn = 0;
 	
 	public void roll(int nbQuilles) {
-		this.score += nbQuilles;
+		
+		score = score + (nbQuilles * multiplier);	// Update score
+		
+		if (turn%2 == 0 && (last_throw + nbQuilles  == 10)) {	// Spare
+			multiplier_turn +=1;
+			multiplier = 2;
+		}
+		
+		last_throw = nbQuilles;
+		
+		if(multiplier_turn>0)					// Update bonus
+			multiplier_turn -=1;
+		else {
+			multiplier = 1;
+		}
+		turn +=1;							//Update turn
 	}
 
 	public int score() {
